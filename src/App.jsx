a34574e9d889cacc585901e743d2b02401732ad8
Daniella Penna <daniella.spenna@gmail.com>
2026-06-05 10:56:38 -0300

    fix: useEffect do react nao do router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate, useEffect } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useEffect } from 'react'
 import { AuthProvider, useAuth } from './hooks/useAuth'
 import Login from './pages/Login'
 import Layout from './components/layout/Layout'
